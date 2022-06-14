a79806622607405683c9d1bb16965cf6dadd7ed5
Tatyana Dmitrieva <visyar76@gmail.com>
2022-06-14 14:52:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrise = 5000;
+        int milePrise = 20;
+        int miles = ticketPrise / milePrise;
+        System.out.println("Вам будет начислено " + miles + " миль за купленный билет.");
+    }
+}
